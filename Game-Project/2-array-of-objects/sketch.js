12fc8ab99752e6be88962fc8f090ce96747165e2
Teresa <teresamt1702@gmail.com>
2019-05-09 09:47:00 -0700

    Changed the archery target board and made the target move with the mouse, currently there are errors because  I am trying to make the target move with keypress

--- Game-Project/2-array-of-objects/sketch.js
+++ Game-Project/2-array-of-objects/sketch.js
@@ -1,51 +1,19 @@
 
-//create an empty array called balls
-
-let balls = [];
-let mySound;
-
-function setup() {
-  createCanvas(800, 400);
-
-}
-function preload {
-  soundformats('mp3','ogg');
-  mySound = loadSound('laser.wav')
-}
-function draw(){
-	background(69, 196, 47);
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	    balls[i].drawBall();
-      balls[i].moveBall();
-	  }
-}
-
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(100, 100);
-  balls.push(b);
-  console.log(balls);
-}
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	constructor(x,y){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-	}
-
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill(195, 42, 237);
-		    triangle(this.x,this.y,this.x-10,this.y+10,this.x+10,this.y+10);
-	}
-
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
-	}
-
-
-}
+ function draw(){
+ createCanvas(600, 600);
+ background(52, 173, 60);
+ ellipse(300, 300, 155, 145);
+ fill("blue")
+ ellipse(300, 300, 135, 125);
+ fill(238, 242, 0)
+ ellipse(300, 300, 115, 105);
+ fill("blue")
+ ellipse(300, 300, 95, 95);
+ fill("red")
+ ellipse(300, 300, 75, 75);
+ fill(238, 242, 0)
+ ellipse(300, 300, 55, 55);
+ fill("blue")
+ line(300, 300, 307, 312);
+ line(300, 300, 293, 398);
+ line(300, 300, 293, 312);
--- Game-Project/empty-example/sketch.js
+++ Game-Project/empty-example/sketch.js
@@ -1,9 +1,44 @@
+function setup(){
+createCanvas(600, 600);
 
-function setup() {
-  createCanvas(600, 600);
 }
 
-function draw(){
-	background(220,100,3);
 
+function target (x,y){
+  background(52, 173, 60);
+  target(mousex, mouseY);
+  arrow();
+
+
+}
+
+function target(x,y){
+  ellipse(x, y, 195, 195);
+  fill(0, 0, 0)
+  ellipse(x, y  , 175, 175);
+  fill(0, 0, 0)
+  stroke(255, 255, 255)
+  ellipse(x, y, 155, 145);
+  fill("blue")
+  ellipse(x, y, 135, 125);
+  fill("blue")
+  ellipse(x, y, 115, 105);
+  fill("red")
+  ellipse(x, y, 95, 95);
+  fill("red")
+  ellipse(x, y, 75, 75);
+  fill(238, 242, 0)
+  ellipse(x, y, 55, 55);
+  fill(238, 242, 0)
+  ellipse(x, y, 35, 35);
+  fill(255, 255, 255);
+
+}
+
+function arrow(){
+stroke(0, 0, 0)
+line(300, 300, 307, 312);
+line(300, 300, 293, 398);
+line(300, 300, 293, 312);
+stroke(0, 0, 0)
 }
